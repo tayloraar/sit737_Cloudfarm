44606aaa26621a13cd158e29bc4117a4130f9820
Aaron <tayloraar@deakin.edu.au>
2021-04-15 14:58:17 +1000

    log out log in redirect

--- app.js
+++ app.js
@@ -17,29 +17,37 @@ app.use(passport.session());
 passport.serializeUser((user, cb)=> cb(null,user));
 passport.deserializeUser((user, cb)=> cb(null, user));
 
-//passport.serializeUser(function(user, cb) {cb(null, user);});
-//passport.deserializeUser(function(obj, cb) {cb(null, obj);});
 
 passport.use(new WebAppStrategy({
   tenantId: "1c02a05d-5037-47f8-9c13-edcb5e7ebe2d",
   clientId: "460d69ad-292c-4454-a65c-9a0575a23696",
   secret: "YzBiMjI2ODctYjE0NC00MDI5LThmNzEtYWU0YWIzMmM2YjEw",
   oauthServerUrl: "https://au-syd.appid.cloud.ibm.com/oauth/v4/1c02a05d-5037-47f8-9c13-edcb5e7ebe2d",
-  redirectUri: "https://cloudfarm.us-south.cf.appdomain.cloud/appid/callback"
+  //redirectUri: "https://cloudfarm.us-south.cf.appdomain.cloud/appid/callback"
+  redirectUri: "http://localhost:8080/appid/callback"
 }));
 
-app.get('/appid/callback', passport.authenticate(WebAppStrategy.STRATEGY_NAME));
+// app.get('/appid/callback', passport.authenticate(WebAppStrategy.STRATEGY_NAME));
 
-app.use(passport.authenticate(WebAppStrategy.STRATEGY_NAME));
+app.get('/appid/callback', function(req,res){
+res.redirect('/dashboard.html');
+});
+
+// app.get('/appid/login', passport.authenticate(WebAppStrategy.STRATEGY_NAME, {
+//  successRedirect: "/dashboard.html"
+// }))
+
+app.get('/appid/login', passport.authenticate(WebAppStrategy.STRATEGY_NAME))
+
+app.get('/appid/logout', function(req,res){
+  WebAppStrategy.logout(req);
+  res.redirect('/');
+})
+
+//app.use(passport.authenticate(WebAppStrategy.STRATEGY_NAME));
 
 app.use(express.static('./public'));
 
-
-//app.get('/protected_resource', passport.authenticate(WebAppStrategy.STRATEGY_NAME), function(req, res) {res.json(req.user); });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
 var port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Example app isn't listening at http://localhost:${port}`)
